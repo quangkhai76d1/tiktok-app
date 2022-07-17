68fc5f7bf85e69ab97966ea3db46d454ca048ee3
quangkhai76d1 <18520876@gm.uit.edu.vn>
2022-07-17 22:57:28 +0700

    make Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { DefaultLayout } from './components/Layout';
+import { publicRoutes } from './Routes';
+
 function App() {
-    return <h1>Reac App</h1>;
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Routes/index.js
@@ -0,0 +1,20 @@
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+import { HeaderOnly } from '~/components/Layout';
+
+//Public Routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/search', component: Search, layout: null },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+//Private Routes
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
